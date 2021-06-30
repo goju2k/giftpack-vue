22cca185b3f66f710cf9ddf02e71c66bdd8a4b2a
goju2k <goju2k@naver.com>
2021-07-01 08:51:29 +0900

    use plugin

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import GiftPackVue from './components'
 
 const app = createApp(App)
 
-//Component Global Registration
-const ctx = require.context('./components', true, /\.vue$/);
-
-ctx.keys().forEach(key => {
-    const regInfo = key.match(/\/(\w+)\.vue/);
-    const comName = regInfo?regInfo[1]:null;
-    if(comName){
-        app.component(comName, ctx(key).default);
-        console.log('[vue-cli main] Global Component Name:('+comName+') Registered.');
-    }
-});
+app.use(
+    GiftPackVue,
+    //{ useList: ['GpLayout'] } //options.userList
+);
 
 app.mount('#app')
